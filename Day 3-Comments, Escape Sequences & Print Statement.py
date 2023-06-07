f918fedce08d77c4bc3ef9086cab01fd0cbebed6
Parth Panchal <134959105+ParthPanchal97@users.noreply.github.com>
2023-06-07 21:41:00 +0530

    Day 3

---
+++ Day 3-Comments, Escape Sequences & Print Statement.py
@@ -0,0 +1,14 @@
+"""
+Hey Parth, Please dont remove this line
+Author: Parth
+"""
+print("Hey I\'m a \"good boy\"\nand this viewer is also a good boy/girl")
+
+print("Hello World")
+#This is a 'Single-Line Comment'
+
+print("This is a print statement.")
+print("Hello World!!!")#Printing Hello World
+
+print("Hey",6,7, sep="~", end="009\n")
+print("Parth")
