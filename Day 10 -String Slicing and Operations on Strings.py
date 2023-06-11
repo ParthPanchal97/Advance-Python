9150b352908ee75162532ec0441f6b746b29bc23
Parth Panchal <134959105+ParthPanchal97@users.noreply.github.com>
2023-06-11 09:03:06 +0530

    Day 10

---
+++ Day 10 -String Slicing and Operations on Strings.py
@@ -0,0 +1,14 @@
+name="Parth, Sohum"
+print(len(name))
+print(name[0:5])
+
+fruit="Mango"
+len1=len(fruit)
+print("Mango is a", len1, "letter word.")
+print(fruit[0:4]) #including 0 but not 4
+print(fruit[1:4]) #including but not 4
+print(fruit[:5]) 
+print(fruit[0:len(fruit)-3])
+print(fruit[0:-3])
+print(fruit[-3:-1])
+
