db6542968a90d0eba262fcad10745c92e8b95e79
Parth Panchal <134959105+ParthPanchal97@users.noreply.github.com>
2023-06-13 18:27:41 +0530

    Day 13

---
+++ Day 13-Elif Conditional Statements.py
@@ -0,0 +1,22 @@
+
+applePrice=10
+budget=200
+if(budget-applePrice>50):
+    print("Alexa, add 1kg Apples to the cart.")
+elif(budget-applePrice>70):
+    print("Its okay you can buy")
+else:
+    print("Alexa, do not add Apples to the cart.")
+    
+num=int(input("Enter a value: "))
+print(num)
+if(num>0):
+    print("Number is positive")
+elif(num==0):
+    print("Number is null")
+elif(num==999):
+    print("Number is special")
+else:
+    print("Number is nagative")
+
+print("I am Happy now")
