724657dc01c3191ad946c2b5868269437d9b3ff6
Parth Panchal <134959105+ParthPanchal97@users.noreply.github.com>
2023-06-17 01:23:33 +0530

    Day 20

---
+++ Day 20-Functions.py
@@ -0,0 +1,29 @@
+def calculateGmean(a,b):
+    mean=(a*b)/(a+b)
+    print(mean)
+def isGreater(a,b):
+    if(a>b):
+        print("First number is greater")
+    else:
+        print("Second number is greater or equal")
+def isLesser(a,b):
+    pass        
+
+a=9
+b=8
+
+# gmean = (a*b)/(a+b)
+# print(gmean)
+calculateGmean(a,b)
+isGreater(a,b)
+
+c=8
+d=7
+# if(c>d):
+#     print("First number is greater")
+# else:
+#     print("Second number is greater or equal")
+# gmean2=(c*d)/(c+d)
+# print(gmean2)
+calculateGmean(c,d)
+isGreater(c,d)
